f70119bd93c86e960ed260b012026d95615909bc
Dmantech21 <dmangold21@gmail.com>
2018-07-07 23:01:24 -0400

    All Done Except Match importer

---
+++ Pits/checkin.php
@@ -0,0 +1,59 @@
+<table class="tableBodyScroll">
+    <thead>
+        <tr>
+            <th style="text-align:center;">Team Number</th>
+            <th style="text-align:center;">Checked-In</th>
+            <th style="text-align:center;">Passed Robot Inspection</th>
+            <th style="text-align:center;">Passed Field Inspection</th>
+            <th style="text-align:center;">Ready For Judging</th>
+        </tr>
+    </thead>
+    
+    <?php
+        session_start();
+
+        $dbLocation = $_SESSION['location'];
+        $dbUser = $_SESSION['dbUser'];
+        $dbPassword = $_SESSION['dbPassword'];
+        $dbName = $_SESSION['dbName'];
+
+        $conn = new mysqli($dbLocation, $dbUser, $dbPassword, $dbName);
+        
+        $results = $conn->query("SELECT * FROM TeamEvent AS te JOIN Event AS e ON te.EventId = e.Id WHERE e.Open = 1 ORDER BY te.TeamId ASC;");
+        $teams = array();
+        while($rs = $results->fetch_array(MYSQLI_ASSOC)) {
+            $teams[] = $rs;
+        }
+
+        foreach($teams as $team) {
+            echo('<tr>'
+                    .'<td>' . $team['TeamId'] . '</td>');
+
+            if($team['IsCheckedIn'] == 1) {
+                echo('<td><i class="material-icons" >check_circle</i></td>');
+            } else {
+                echo('<td><i class="material-icons" >close</i></td>'); 
+            }
+
+            if($team['PassedRobotInspection'] == 1) {
+                echo('<td><i class="material-icons" >check_circle</i></td>');
+            } else {
+                echo('<td><i class="material-icons" >close</i></td>'); 
+            }
+
+            if($team['PassedFieldInspection'] == 1) {
+                echo('<td><i class="material-icons" >check_circle</i></td>');
+            } else {
+                echo('<td><i class="material-icons" >close</i></td>'); 
+            }
+
+            if($team['ReadyForJudging'] == 1) {
+                echo('<td><i class="material-icons" >check_circle</i></td>');
+            } else {
+                echo('<td><i class="material-icons" >close</i></td>'); 
+            }
+
+            echo('</tr>');
+        }
+    ?>
+</table>
--- Queueing/updateMatch.php
+++ Queueing/updateMatch.php
@@ -2,8 +2,9 @@
     header("Content-type: application/json");
     session_start();
 
-    $teamNumber = $_GET['teamNumber'];
-    $teamName = $_GET['teamName'];
+    $eventId = $_GET['eventId'];
+    $matchNumber = $_GET['matchNumber'];
+    $attribute = $_GET['attribute'];
 
     $dbLocation = $_SESSION['location'];
     $dbUser = $_SESSION['dbUser'];
@@ -12,11 +13,9 @@
 
     $conn = new mysqli($dbLocation, $dbUser, $dbPassword, $dbName);
 
-    $conn->query("INSERT INTO  Team (TeamNumber, Name)
-        VALUES (
-        $teamNumber, '$teamName'
-        );");
+    $result = $conn->query("UPDATE EventMatch SET $attribute = 1 WHERE MatchNumber = $matchNumber AND EventId = $eventId;");
     
     echo("{}");
+
     $conn->close();
 ?>
