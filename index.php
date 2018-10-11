2358a86327178988de53ca3a11769a5d8027a196
Mikaela Caron <mikaelacaron@gmail.com>
2018-10-11 15:40:45 -0500

    Delete all files, starting over
    Modify Login.php and create main.css for entire style

--- index.php
+++ index.php
@@ -1,73 +1,25 @@
+<?php
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="./index.css">
-    <body>
-        <?php
-        session_start();
-        $conn = new mysqli("localhost", "phpmyadmin", "IndianaFIRST", "phpmyadmin");
+<head>
+	<title>Login Page</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
 
-        if ($_SESSION['timeout'] + 600 < time()) {
-            $_SESSION['logged_In'] = false;
-            $_SESSION['userName'] = null;
-            $_SESSION['Role'] = null;
-            $_SESSION['timeout'] = null;
-        } else {
-            $_SESSION['timeout'] = time();
-        }
-        ?>
-        <script>
-        let user;
-        $( document ).ready(function() {
-            $.get(`./login.php?checkLoggedIn=true`, function(result){
-            user = result;
-            });
-        });
+	<main>
+		<div id="login">
+			<input type="text" id="user" value="Username"></input>
+			<input type="password" id="psw" value="password"></input>
+			<br />
+			<button class="button button-fade" type="submit" onclick="login()">Login</button>
+		</div>
+    </main>
 
-        function login() {
-            let oldUser = user;
-            let userName = $("#user").val();
-            let password = $('#psw').val();
-            $.get(`./login.php?checkedLoggedIn=false&userName=${userName}&password=${password}`, function(result){
-                user = result;
-                if (user !== oldUser) {
-                    if (user.Role === 'Admin') {
-                        window.location.href = './Admin/admin.php';
-                    } else if(user.Role === 'RobotInspector' || 
-                            user.Role === 'FieldInspector' ||
-                            user.Role === 'Receptionist' ||
-                            user.Role === 'Judging') {
-                        window.location.href = './BasicUsers/checkin.php';
-                    } else if (user.Role === 'Queuer') {
-                        window.location.href = './Queueing/queueing.php';
-                    } else if (user.Role === 'Guest') {
-                        window.location.href = './Pits/competition.php'
-                    } else {
-                        window.location.reload();
-                    }
-                }
-            });
-        }
-
-        function logout() {
-            $.get(`./logout.php`, function(result){
-            window.location.reload();
-            });
-        }
-        </script>
-        <div class="login" flex>
-            <div>
-                <label class="large-font"> User Name: &nbsp; </label>
-                <input class="medium-font" type="text" id="user"></input>
-            </div>
-            </br>
-            <div>
-                <label class="large-font">Password: &nbsp; &nbsp; </label>
-                <input class="medium-font" type="password" id="psw"></input>
-            </div>
-            </br>
-            <button class="button" type="submit" onclick="login()">Login</button>
-        </div>
-    </body>
+</body>
 </html>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    $_SESSION['logged_In'] = false;
-    $_SESSION['userName'] = null;
-    $_SESSION['Role'] = null;
-    $_SESSION['timeout'] = null;
-?>
