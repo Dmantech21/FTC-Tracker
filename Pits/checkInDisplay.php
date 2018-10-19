d481e075979a845b0226504ba5e15675a0ab5249
Mikaela Caron <mikaelacaron@gmail.com>
2018-10-18 22:35:26 -0500

    Add scrollbar to chekInDisplay
    TODO: make scroll automatic

--- Pits/checkInDisplay.php
+++ Pits/checkInDisplay.php
@@ -92,3 +92,18 @@
 
 </body>
 </html>
+
+
+<script>
+    var $el = $("#displayTable");
+    function anim() {
+    var st = $el.scrollTop();
+    var sb = $el.prop("scrollHeight")-$el.innerHeight();
+    $el.animate({scrollTop: st<sb/2 ? sb : 0}, 4000, anim);
+    }
+    function stop(){
+    $el.stop();
+    }
+    anim();
+    $el.hover(stop, anim);
+</script>
