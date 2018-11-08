2f6c37b1368e78bec1471cd02e42392b4b10d98b
Mikaela Caron <mikaelacaron@gmail.com>
2018-11-08 00:56:50 -0500

    Correctly displays matchViewDisplay for ONLY the first 4 matches
    TODO: correctly display all other matches after the first 4

--- Pits/matchViewDisplay.php
+++ Pits/matchViewDisplay.php
@@ -22,7 +22,6 @@
 
 	<link rel="stylesheet" type="text/css" href="../main.css">
     <link rel="stylesheet" type="text/css" href="display.css">
- 	<!-- <link rel="stylesheet" type="text/css" href="matchPlayDisplay.css">-->
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
@@ -31,6 +30,67 @@
 	<main>
 
 		<?php include '../headerDisplay.php'; ?>
+
+        <div class="card-holder">
+            <?php
+
+                $matchesToShow = $uncompletedMatchCount >= 4 ? 4 : $uncompletedMatchCount;
+                for($i = 0; $i < $matchesToShow; $i++) {
+
+                    $matchLoop=$matches[$i];
+
+                    echo('<div class="card slot' . $matchLoop['MatchNumber'] . '">');
+
+                        echo('<div class="container">');
+                            echo('<h3 class="titleMatch">Match ' . $matchLoop['MatchNumber']);
+                            if($i == 0) {
+                                echo(' - Current Match');
+                            } else if ($i == 1) {
+                                echo(' - On Field Waiting ');
+                            }echo('</h3>');
+
+                            echo('<div class="teamRed red1">');
+                                echo('Red 1: ' . $matchLoop['Red1']);
+                                echo('<br>');
+                                if($matchLoop['Red1Queued'] == 1) {
+                                    echo('<img src="../images/icons8-checkmark-filled-100-white.png" class="checkImg" alt="check">');
+                                }
+                            echo('</div>');//end teamRed red1
+
+                            echo('<div class="teamRed red2">');
+                                echo('Red 1: ' . $matchLoop['Red2']);
+                                echo('<br>');
+                                if($matchLoop['Red2Queued'] == 1) {
+                                    echo('<img src="../images/icons8-checkmark-filled-100-white.png" class="checkImg" alt="check">');
+                                }
+                            echo('</div>');//end teamRed red2
+
+                            echo('<div class="teamBlue blue1">');
+                                echo('Blue 1: ' . $matchLoop['Blue1']);
+                                echo('<br>');
+                                if($matchLoop['Blue1Queued'] == 1) {
+                                    echo('<img src="../images/icons8-checkmark-filled-100-white.png" class="checkImg" alt="check">');
+                                }
+                            echo('</div>');//end teamBlue blue1
+
+                            echo('<div class="teamBlue blue2">');
+                                echo('Blue 2: ' . $matchLoop['Blue2']);
+                                echo('<br>');
+                                if($matchLoop['Blue2Queued'] == 1) {
+                                    echo('<img src="../images/icons8-checkmark-filled-100-white.png" class="checkImg" alt="check">');
+                                }
+                            echo('</div>');//end teamBlue blue1
+
+                        echo ('</div>');//end container
+
+                    echo('</div>');//end card and card-holder
+
+
+
+                } //end for loop
+
+            ?><!-- end php script-->
+        </div><!-- end card-holder -->
 
 
     </main>
